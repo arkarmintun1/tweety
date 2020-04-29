6db5c4e3262b5a82e97ffef9b659f3e62d1cd2b0
Arkar Min Tun <arkarmintun1@gmail.com>
2020-04-29 22:04:17 +0630

    design the timeline

---
+++ resources/views/_publish-tweet-panel.blade.php
@@ -0,0 +1,11 @@
+<div class="border border-blue-400 rounded-lg px-8 py-6 mb-8">
+    <form action="">
+        <textarea name="body" id="body" class="w-full" placeholder="What's up doc?"></textarea>
+        <hr class="my-4">
+        <footer class="flex justify-between">
+            <img src=" https://api.adorable.io/avatars/40/abott@adorable.png" alt="" class="rounded-full mr-2">
+            <button type="submit" class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white">Tweet-a-roo!</button>
+        </footer>
+
+    </form>
+</div>
---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,9 @@
+<div class="flex p-4 border-b border-b-gray-400">
+    <div class="mr-2 flex-shrink-0">
+        <img src=" https://api.adorable.io/avatars/50/abott@adorable.png" alt="" class="rounded-full mr-2">
+    </div>
+    <div>
+        <h5 class="font-bold mb-4">John Doe</h5>
+        <p class="text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet assumenda, consequuntur dicta ducimus eligendi eos id impedit libero maxime nam neque nesciunt odit quibusdam reiciendis suscipit vel voluptatibus? Labore, nihil!</p>
+    </div>
+</div>
