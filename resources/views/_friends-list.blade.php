0fbbf68fdd6db538bb889ea2287a1e37f92fa016
Arkar Min Tun <arkarmintun1@gmail.com>
2020-05-02 20:40:19 +0630

    build a following

--- resources/views/_friends-list.blade.php
+++ resources/views/_friends-list.blade.php
@@ -1,12 +1,15 @@
-<h3 class="font-bold text-xl mb-4">Friends</h3>
+<div class="bg-gray-200 rounded-lg py-4 px-6">
+    <h3 class="font-bold text-xl mb-4">Following</h3>
 
-<ul>
-    @foreach(range(1, 8) as $index)
-    <li class="mb-4">
-        <div class="flex items-center text-sm">
-            <img src=" https://api.adorable.io/avatars/40/abott@adorable.png" alt="" class="rounded-full mr-2">
-            John Doe
-        </div>
-    </li>
-    @endforeach
-</ul>
+    <ul>
+        @foreach(auth()->user()->follows as $user)
+            <li class="mb-4">
+                <div class="flex items-center text-sm">
+                    <img src="{{ $user->avatar }}" alt="" class="rounded-full mr-2">
+                    {{ $user->name }}
+                </div>
+            </li>
+        @endforeach
+    </ul>
+
+</div>
